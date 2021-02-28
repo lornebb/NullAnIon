da09dfcbd67cb76a674c6ffd626cb2b3afb804f8
lornebb <51881266+lornebb@users.noreply.github.com>
2021-02-28 19:27:10 +0100

    added library template folder adn html file. added library views.py, urls and added library url to main urls.py

--- library/views.py
+++ library/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Song
 
 # Create your views here.
+
+def library(request):
+    """ A view to return library page and songs from library """
+
+    song = Song.objects.all()
+
+    context = {
+        'song': song,
+    }
+
+    return render(request, 'library/library.html', context)
