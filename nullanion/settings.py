847ef41cfe831010d57c9e8fd1de7950f8f88ca4
lornebb <51881266+lornebb@users.noreply.github.com>
2021-03-01 20:43:59 +0100

    deployment to heroku postgresql

--- nullanion/settings.py
+++ nullanion/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
+import dj_database_url
 
 from pathlib import Path
 
@@ -110,7 +111,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
