25f84d37d36a76fae9a7042f0a89e5358b2cbe16
lornebb <51881266+lornebb@users.noreply.github.com>
2021-03-01 17:15:58 +0100

    updated library models to corect spelling mistake, and include other image options. updated migrations to those changes in models. added images loaded from website for album artworks. added .svg of play icon

--- library/models.py
+++ library/models.py
@@ -17,13 +17,15 @@ class Category(models.Model):
         return self.friendly_name
 
 class Song(models.Model):
-    categrory = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     song_name = models.CharField(max_length=254)
     album_name = models.CharField(max_length=254, null=True, blank=True)
     artist_name = models.CharField(max_length=254, null=True, blank=True)
     year = models.IntegerField(null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    audio_link = models.URLField(max_length=1024)
+    description = models.CharField(max_length=1024)
 
     def __str__(self):
         return self.song_name
