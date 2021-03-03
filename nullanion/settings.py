1ba10e9a834ac6aa7b57a53555ae6a8cd3f6be4b
lornebb <51881266+lornebb@users.noreply.github.com>
2021-03-03 16:27:20 +0100

    fixed variable logic in settings to get env vars. added aws hosting routes and static file set up

--- nullanion/settings.py
+++ nullanion/settings.py
@@ -15,6 +15,9 @@ import dj_database_url
 
 from pathlib import Path
 
+if os.path.exists("env.py"):
+    import env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['nullanion.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['nullanion.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
