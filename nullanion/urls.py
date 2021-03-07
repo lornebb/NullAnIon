3317c8a66d0c681177655646b25a45086d7d2f0c
lornebb <51881266+lornebb@users.noreply.github.com>
2021-03-07 15:53:00 +0100

    further profiles app code - html templates made and starter models created

--- nullanion/urls.py
+++ nullanion/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
-    path('library', include('library.urls')),
-    path('services', include('services.urls')),
+    path('library/', include('library.urls')),
+    path('services/', include('services.urls')),
+    path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
