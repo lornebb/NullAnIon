045bf6004dbe6e4a9d57ed95df2f6841e8ceec81
lornebb <51881266+lornebb@users.noreply.github.com>
2021-03-08 23:09:16 +0100

    added migrations for userprofile and profile models

--- profiles/models.py
+++ profiles/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+# from typing_extensions import Required
 
 class UserProfile(models.Model):
     """
@@ -9,6 +10,7 @@ class UserProfile(models.Model):
     order history.
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField(max_length=254, null=False, blank=False)
 
     def __str__(self):
         return self.user.username
