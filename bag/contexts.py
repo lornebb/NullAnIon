75321708a7d44d5b0f9d64c858e9202cc41805d2
lornebb <51881266+lornebb@users.noreply.github.com>
2021-03-12 14:11:43 +0100

    added bag context and now passing bag contents to checkout page

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,11 +1,27 @@
-from decimal import Decimal
+# from decimal import Decimal
 # from django.conf import settings
+from django.shortcuts import get_object_or_404
+from services.models import Mix, Master, Production
 
 def bag_contents(request):
 
     bag_items = []
-    total = 0
+    total = 123.32
     product_count = 0
+    grand_total = total
+    bag = request.session.get('bag', {})
+
+    for order_type, quantity in bag.items():
+        # product = get_object_or_404(Production, pk=order_type)
+        # if bag.order_type == Mix, Master:
+            # total += product.price
+        # product_count += quantity
+        bag_items.append({
+            'order_type' : order_type,
+            'quantity' : quantity,
+            # 'product' : product,
+        })
+
     grand_total = total
 
     context = {
