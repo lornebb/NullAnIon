e9a8fcaec00dfaa96bacd51825c65cc691180f11
lornebb <51881266+lornebb@users.noreply.github.com>
2021-03-12 15:47:07 +0100

    updated contexts in bag, and some checkout logic for displaying checkout form

--- bag/contexts.py
+++ bag/contexts.py
@@ -6,28 +6,29 @@ from services.models import Mix, Master, Production
 def bag_contents(request):
 
     bag_items = []
-    total = 123.32
-    product_count = 0
+    total = 0
+    order_count = 0
     grand_total = total
     bag = request.session.get('bag', {})
 
-    for order_type, quantity in bag.items():
-        # product = get_object_or_404(Production, pk=order_type)
-        # if bag.order_type == Mix, Master:
-            # total += product.price
-        # product_count += quantity
+    for item_id, item_data in bag.items():
+        # if isinstance(item_data, int):
+        order = get_object_or_404(Production, pk=item_id)
+        total = item_data * order.price
+        order_count += item_data
         bag_items.append({
-            'order_type' : order_type,
-            'quantity' : quantity,
-            # 'product' : product,
+            'order' : order,
+            'item_id' : item_id,
+            'quantity' : item_data
         })
+        
 
     grand_total = total
 
     context = {
         'bag_items': bag_items,
         'total': total,
-        'product_count': product_count,
+        'order_count': order_count,
         'grand_total': grand_total,
     }
 
