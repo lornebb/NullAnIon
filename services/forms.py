42c0870f3a061dd5315c143590e67a2cdcd186a7
lornebb <51881266+lornebb@users.noreply.github.com>
2021-03-21 21:07:55 +0100

    testing styling for forms in services

--- services/forms.py
+++ services/forms.py
@@ -1,6 +1,9 @@
-from django.forms import ModelForm, Textarea, RadioSelect
+from django.forms import ModelForm, Textarea, RadioSelect, DateInput
 from django.forms.widgets import CheckboxSelectMultiple
 from services.models import Mix, Master, Production
+
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit, Row, Column, Div
 
 
 class MixForm(ModelForm):
@@ -17,8 +20,30 @@ class MixForm(ModelForm):
             'stem_choices': RadioSelect(),
             'revisions': RadioSelect(),
             'mix_extras': CheckboxSelectMultiple(),
-            # 'deliver_by': DateField(),
+            'deliver_by': DateInput(),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Div('package_type', style="background: white;", title="Explication title", css_class="control")
+            # Row(
+            #     Column('package_type', css_class='icon-single HELLO WORLD'),
+            #     Column('stem_choices', css_class='coioioioioi'),
+            #     css_class='form-row'
+            # ),
+            # 'address_1',
+            # 'address_2',
+            # Row(
+                # Column('city', css_class='form-group col-md-6 mb-0'),
+                # Column('state', css_class='form-group col-md-4 mb-0'),
+                # Column('zip_code', css_class='form-group col-md-2 mb-0'),
+                # css_class='form-row'
+            # ),
+            # 'check_me_out',
+            # Submit('submit', 'Sign in')
+        )
 
 
 class MasterForm(ModelForm):
