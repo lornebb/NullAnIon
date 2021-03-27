637fbff94af565b7219b4c23870fd42846c76883
lornebb <51881266+lornebb@users.noreply.github.com>
2021-03-28 00:00:43 +0100

    migrations and updates to user profile model and views

--- profiles/models.py
+++ profiles/models.py
@@ -10,7 +10,8 @@ class UserProfile(models.Model):
     order history.
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    email = models.EmailField(max_length=254, null=False, blank=False)
+    default_email = models.EmailField(max_length=254, null=False, blank=False)
+    default_phone_number = models.CharField(max_length=15, null=False, blank=False)
 
     def __str__(self):
         return self.user.username
