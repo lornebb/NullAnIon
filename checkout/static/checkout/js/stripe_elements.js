53ad0e4f868b5fbe715868011f652cac3d79a51a
lornebb <51881266+lornebb@users.noreply.github.com>
2021-03-28 20:24:03 +0200

    minor styling changes

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -44,7 +44,6 @@ card.addEventListener("change", function (event) {
 var form = document.getElementById('payment-form');
 
 form.addEventListener('submit', function (ev) {
-  console.log(clientSecret)
   ev.preventDefault();
   card.update({ 'disabled': true });
   $('#submit-button').attr('disabled', true);
