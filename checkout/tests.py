dc1a67a87356cae66ce8e565ea34baa6200b3cba
lornebb <51881266+lornebb@users.noreply.github.com>
2021-03-28 18:35:52 +0200

    added testing concepts to checkout

--- checkout/tests.py
+++ checkout/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .forms import OrderForm
 
-# Create your tests here.
+
+class OrderTest(TestCase):
+    def setUp(self):
+        OrderForm.objects.create(
+            full_name="Testy McTestFace",
+            email="testy@test.face",
+            phone_number="99999999",
+        )
+
+    def test_review_content_is_required(self):
+        form = OrderForm({'review_content': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('full_name', form.errors.keys())
+        self.assertEqual(form.errors['full_name'], 'This field is required.')
+
+    def order_works(self):
+        """Order forms that are valid work"""
+        form = OrderForm()
+        self.assertNotEquals(form.Meta.exclude, ['full_name', 'email'])
