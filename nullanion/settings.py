a181e30655c5fe9634a9ee9bfc01ac7a8b357719
Lorne Ashley <lorne.b.b@gmail.com>
2021-03-29 15:53:43 +0000

    small update to settings file about default email address for dev mode

--- nullanion/settings.py
+++ nullanion/settings.py
@@ -207,7 +207,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'info@nullanion.com'
+    DEFAULT_FROM_EMAIL = 'lorne.b.b@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
